2e25d08a11252a88af7b97a42c4e9e308bf9371a
abharish480 <abharish480@gmail.com>
2024-10-15 23:44:30 +0530

    Adding cypress test script

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  video:false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    videoOnFailOnly:false
+  },
   e2e: {
+    baseUrl: 'https://naveenautomationlabs.com/opencart/index.php?route=account/login',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
   env:{
